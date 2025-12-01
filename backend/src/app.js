1520aab5f64c28b312fcd6b48fc94518f9e6b5e4
Reyansh-Manta <mantareyansh@gmail.com>
2025-12-02 04:26:38 +0530

    Initialize Express backend

---
+++ backend/src/app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+const app = express()
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true,
+    limit: "10000kb"
+}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+app.get("/", (req, res) => {
+  res.send("Backend is live ✅");
+});
+
+export {app}
---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import connectDB from "./db/index.js"
+import { app } from "./app.js"
+
+connectDB()
+.then(() => {
+    
+    app.on("error", (err) => {
+        console.log("App connection error ", err)
+            throw err;
+        })
+        const PORTT = process.env.PORT || 4000
+        app.listen(PORTT, () => {
+            console.log(`Server running on port ${PORTT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("Error in connecting to app", err);
+    })
